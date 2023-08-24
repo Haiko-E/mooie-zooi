0243086d50eb29b2cc4c44e2c227f015a7fda354
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2023-08-24 11:13:18 +0200

    added swiper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,33 +1,47 @@
-type Type = 'subComponent' | 'imageComponent' | 'textComponent';
+export type Type =
+  | 'subComponent'
+  | 'imageComponent'
+  | 'textComponent'
+  | 'adressComponent';
 
-type Image = {
+export interface Image {
   image: {};
   _type: 'imageComponent';
   _key: string;
-};
+}
 
-type Sub = {
+export interface Sub {
   subtitle: string;
   _type: 'subComponent';
   _key: string;
-};
+}
 
-type Text = {
+export interface Text {
   text: string;
   _type: 'textComponent';
   _key: string;
-};
-type Component = Image | Sub | Text;
+}
 
-export function getComponent(_type: Type, components: Component[]) {
+export interface Address {
+  _type: 'adressComponent';
+  _key: string;
+  street: string;
+  zip: string;
+  city: string;
+  email: string;
+  website: string;
+  telephone: string;
+}
+//prettier-ignore
+export type Component<T> = 
+  T extends 'imageComponent'? Image:
+  T extends 'subComponent'? Sub:
+  T extends 'textComponent'? Text: 
+  T extends 'adressComponent' ? Address:
+  never;
+
+//prettier-ignore
+export function getComponent<T extends Type>(_type: T, components: (Image | Text | Sub | Address)[]) {
   const result = components.find((comp) => comp._type === _type);
-  if (result._type === 'subComponent') {
-    return result;
-  }
-  if (result._type === 'textComponent') {
-    return result;
-  }
-  if (result._type === 'imageComponent') {
-    return result;
-  }
+  return result as Component<T>;
 }
