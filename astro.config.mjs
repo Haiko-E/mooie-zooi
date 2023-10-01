7d282f1dd646f400f68bcbaae0601827faa121a6
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2023-10-01 23:27:48 +0200

    add env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import { loadEnv } from 'vite';
 import sanity from 'astro-sanity';
+
+const { SANITY_DATASET } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +16,7 @@ export default defineConfig({
     react(),
     sanity({
       projectId: 'gwghkra8',
-      dataset: 'develop',
+      dataset: SANITY_DATASET,
       apiVersion: '2023-08-17',
       useCdn: true,
     }),
